fb006cf2ec36736d3b18f68971f20d702755bbf7
j-castaneda-b <j.castanedabarbosa@gmail.com>
2024-10-31 10:45:36 -0500

    New folder structure, changes in api handler

---
+++ tasks/prompts.py
@@ -0,0 +1,47 @@
+PROMPT_OSI_QA = """
+    Given the following text:
+    ´´´
+    {context}
+    ´´´
+    Generate question/answer pairs relevant to the text you observe. These questions must be formulated in a way such that:
+    a. The question is focused on knowing more about open source software licensing and applications
+    b. The question must have a financial regulatory intent
+    c. The answer distills knowledge in a concise and factual manner in order to answer the question's intent.
+
+    Return them in a numerated list following this format:
+    ´´´
+    1. <question> - <answer>
+    2. <question> - <answer>
+    .
+    .
+    .
+    n. <question> - <answer>
+    ´´´
+    ONLY provide this list, nothing else, nothing extra.
+"""
+
+PROMPT_OSI_ABBREV = """
+    Given the following text:
+    ´´´
+    {context}
+    ´´´
+    Extract all abbreviations that appear along with their expanded versions such that:
+    a. The abbreviations have to do with open source licensing
+    b. The question must have a financial regulatory intent
+    c. The answer distills knowledge in a concise and factual manner in order to answer the question's intent.
+
+    Return them in a numerated list following this format:
+    ´´´
+    1. <question> - <answer>
+    2. <question> - <answer>
+    .
+    .
+    .
+    n. <question> - <answer>
+    ´´´
+    ONLY provide this list, nothing else, nothing extra.
+"""
+
+SYSTEM_PROMPT_OSI = "You are an accurate, articulate and knowledgeable in open source licensing knowledge for financial and business applications."
+
+
